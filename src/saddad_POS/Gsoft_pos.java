260c6d5486b9c6debde68af9a056e478b3cce045
saddadnabbil <saddadnabbil>
2024-06-28 08:35:46 +0700

    first init

---
+++ src/saddad_POS/Gsoft_pos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saddad_POS;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author acer
+ */
+public class Gsoft_pos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login p = new Login();
+        p.setVisible(true);
+    }
+    
+}
